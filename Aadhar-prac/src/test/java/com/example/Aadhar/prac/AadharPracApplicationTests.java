2e15d624ae2973713ca21a36af481f35f153176b
Adesh <adeshsamdadiya@gmail.com>
2023-06-01 11:51:01 +0530

    Assignment Solution

---
+++ Aadhar-prac/src/test/java/com/example/Aadhar/prac/AadharPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aadhar.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadharPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
